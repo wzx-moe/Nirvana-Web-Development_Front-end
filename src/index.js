26d38fdedb30a4e87d8790d809cc964a5d5bad3f
Telatol <65938841+Telatol@users.noreply.github.com>
2022-08-03 15:32:45 +1000

    NWD-16 Configure the router

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AboutPage from './pages/AboutPage';
+import ContactUsPage from './pages/ContactUsPage';
+import DetailPage from './pages/DetailPage';
+import HomePage from './pages/HomePage';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="aboutPage" element={<AboutPage/>}/>
+        <Route path="contactUsPage" element={<ContactUsPage/>}/>
+        <Route path="detailPage" element={<DetailPage/>}/>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
